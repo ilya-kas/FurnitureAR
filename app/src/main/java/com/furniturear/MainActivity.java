ae0fe6febe64f99d91c21ed4ac9af99c4cc243a8
ilya_kas <ilya02.mail@gmail.com>
2020-03-23 17:02:06 +0300

    добавил выезжающее слева меню
    пока что не работает выбор компонентов

--- app/src/main/java/com/furniturear/MainActivity.java
+++ app/src/main/java/com/furniturear/MainActivity.java
@@ -1,6 +1,10 @@
 package com.furniturear;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.app.Activity;
 import android.app.ActivityManager;
@@ -8,19 +12,22 @@ import android.content.Context;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.google.android.material.navigation.NavigationView;
 import com.google.ar.sceneform.ux.ArFragment;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     ArFragment arFragment;
     private static final String TAG = MainActivity.class.getSimpleName();
     private static final double MIN_OPENGL_VERSION = 3.0;
     List<Model> models = new ArrayList<>();
     Model selectedModel;
+    DrawerLayout drawer;
 
     public static boolean checkIsSupportedDeviceOrFinish(final Activity activity) {
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
@@ -47,16 +54,20 @@ public class MainActivity extends AppCompatActivity {
     @SuppressWarnings({"AndroidApiChecker", "FutureReturnValueIgnored"})
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        drawer = findViewById(R.id.drawer_layout);
+
         if (!checkIsSupportedDeviceOrFinish(this)) {
             return;
         }
+
         setContentView(R.layout.activity_main);
         arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.ux_fragment);//это сцена, куда суём модельки
 
-        loadModels();
-        selectedModel = models.get(2);
+        loadModels(); //загружаем все модельки в классы
+        selectedModel = models.get(0);
 
-        selectedModel.resetModelRenderable(this);
+        selectedModel.resetModelRenderable(this); //устанавливаем модельку, которая базово будет отображаться
         selectedModel.setOnTapListener(arFragment);
     }
 
@@ -66,5 +77,12 @@ public class MainActivity extends AppCompatActivity {
         models.add(new Model("Bookcase"));
         models.add(new Model("Desk"));
         models.add(new Model("Desk_01"));
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
